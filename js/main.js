bf2261cfacf7848187e175e1a6f2aef6b57dd086
Carlos Morales <carlosjosuemn98@gmail.com>
2020-05-02 22:01:02 -0500

    Front terminado

--- js/main.js
+++ js/main.js
@@ -1,23 +1,36 @@
-const user = document.getElementById('account');
-const button = document.getElementById('sendUser');
+const checkbox = document.querySelector('#checkbox');
 
-async function getAcces(name) {
-    try {
-        const response = await fetch('/', {
-            method: 'post',
-            body: JSON.stringify({ name }),
-            headers: {
-                'Content-Type': 'application/json',
-            },
-        });
-        const data = await response.json();
-        return data;
-    } catch (error) {
-        console.log(error);
+// if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+//     checkbox.setAttribute('checked', true);
+// } else {
+//     checkbox.setAttribute('checked', false);
+// }
+
+checkbox.addEventListener('change', function () {
+    if (this.checked) {
+        document.body.classList.remove('is-light-mode');
+        document.body.classList.add('is-dark-mode');
+        localStorage.setItem('dark', 'true');
+    } else {
+        document.body.classList.remove('is-dark-mode');
+        document.body.classList.add('is-light-mode');
+        localStorage.setItem('dark', 'false');
+    }
+});
+
+function saveDarkMode() {
+    if (localStorage.getItem('dark')) {
+        const dark = localStorage.getItem('dark');
+
+        if (dark === 'true') {
+            document.body.classList.add('is-dark-mode');
+            document.body.classList.remove('is-light-mode');
+            checkbox.setAttribute('checked', true);
+        } else {
+            document.body.classList.remove('is-dark-mode');
+            document.body.classList.add('is-light-mode');
+        }
     }
 }
 
-button.addEventListener('click', async () => {
-    const data = await getAcces(user.value);
-    console.log(data);
-});
+saveDarkMode();
